d9a09546b36c7746a2cd58e2be18bd5cac1b3380
cioko <brzepat@gmail.com>
2022-05-08 15:21:49 +0200

    Start OSKAPP repo

---
+++ src/test/java/com/example/osk/OskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.osk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
