c479ba425728e20eab931c15e90cbcd75333ba68
DevBuk <mateusz.bukowski@o2.pl>
2022-05-19 15:55:06 +0200

    Widok Kursanci + formularz    (basic)

--- src/main/java/com/example/osk/controller/ClientController.java
+++ src/main/java/com/example/osk/controller/ClientController.java
@@ -8,6 +8,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.view.RedirectView;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @Controller
@@ -20,18 +21,28 @@ public class ClientController {
         this.clientService = clientService;
     }
 
+    // to DELETE - Mateusz
+    @GetMapping("/")
+    public String getIndex() {
+        return "index";
+    }
+
     // getting all the clients for showing on page
     @GetMapping("/clients")
     public String getClientsList(Model model) {
         List<Client> allClients = this.clientService.getAllClients();
         model.addAttribute("client", allClients);
-        return "index";
+        return "clients/clientsList";
     }
+
+
+
+
 
     // only get view for add new client
     @GetMapping("/addClient")
     public String getAddClientView() {
-        return "clients/TODO";
+        return "clients/addNewPerson";
     }
 
     // save client in DB then redirecting a view
@@ -40,4 +51,36 @@ public class ClientController {
         this.clientService.addClient(client);
         return new RedirectView("/clients");
     }
+
+    // to DELETE - Mateusz
+    // get client for only edit view
+    @GetMapping("/editClient/{id}")
+    public String getEditClient(@PathVariable("id") Long id, Model model) {
+        Client client = clientService.getClient(id);
+        model.addAttribute("client", client);
+        return "clients/editPerson";
+    }
+
+    // to DELETE - Mateusz
+    // save edit student
+    @PostMapping("/addClient/{id}")
+    public RedirectView postEditClient(@Valid @ModelAttribute Client newClient, @PathVariable("id") Long id) {
+        clientService.editClient(newClient);
+        return new RedirectView("/editClient/{id}");
+    }
+
+    // to DELETE - Mateusz
+    // delete client
+    @PostMapping("/editClient/{id}")
+    public RedirectView deleteClient(@PathVariable("id") Long id) {
+        clientService.deleteClient(id);
+        return new RedirectView("/clients");
+    }
+
+// to DELETE - Mateusz
+//    @GetMapping("/")
+//    @ResponseBody
+//    public String mateuszBTest(){
+//        return"tesMB";
+//    }
 }
