08e6194b3dc1e4450c92f9096c14248c89cf8bae
cioko <brzepat@gmail.com>
2022-05-22 16:08:33 +0200

    Add two different provider for users

--- src/main/java/com/example/osk/config/WebSecurityConfig.java
+++ src/main/java/com/example/osk/config/WebSecurityConfig.java
@@ -1,6 +1,7 @@
 package com.example.osk.config;
 
 import com.example.osk.service.ClientService;
+import com.example.osk.service.InstructorService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,6 +11,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 
@@ -19,6 +21,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     private ClientService clientService;
+    @Autowired
+    private InstructorService instructorService;
 
     @Bean
     protected BCryptPasswordEncoder passwordEncoder() {
@@ -26,30 +30,38 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
     @Bean
-    public DaoAuthenticationProvider authenticationProvider() {
+    public DaoAuthenticationProvider authenticationClientProvider() {
         DaoAuthenticationProvider auth = new DaoAuthenticationProvider();
         auth.setUserDetailsService(clientService);
         auth.setPasswordEncoder(passwordEncoder());
         return auth;
     }
-
+    @Bean
+    public DaoAuthenticationProvider authenticationInstructorProvider() {
+        DaoAuthenticationProvider auth = new DaoAuthenticationProvider();
+        auth.setUserDetailsService(instructorService);
+        auth.setPasswordEncoder(passwordEncoder());
+        return auth;
+    }
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.authenticationProvider(authenticationProvider());
+        auth.authenticationProvider(authenticationClientProvider());
+        auth.authenticationProvider(authenticationInstructorProvider());
         auth.inMemoryAuthentication()
-                .withUser("test").password(passwordEncoder()
-                        .encode("test")).roles("USER")
+                .withUser("user").password(passwordEncoder()
+                        .encode("user")).roles("USER")
                 .and()
                 .withUser("admin").password(passwordEncoder()
                         .encode("admin")).roles("ADMIN");
     }
+
 
     @Override
     protected void configure(final HttpSecurity http) throws Exception {
         http.authorizeRequests()
                 .antMatchers("/")
                 .hasAnyAuthority("ROLE_USER", "ROLE_ADMIN")
-                .antMatchers( "/clients", "/instructors","/vehicles")
+                .antMatchers("/clients", "/instructors", "/vehicles", "/bookPracticalLesson")
                 .hasAnyAuthority("ROLE_ADMIN")
                 .and()
                 .csrf().disable()
@@ -69,4 +81,5 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
                 .logoutSuccessUrl("/login?logout");
     }
+
 }
